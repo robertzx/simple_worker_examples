f6ba87c270fad011441b61158634760b625f3bf8
Ken Fromm <admin@frommworldwide.com>
2011-05-16 21:41:01 -0700

    Moved SimpleWorker, MongoHQ, and IndexTank keys to a config.yml file and added some additional queue/schedule methods to the runner.

--- lib/indextank.com/mongo_to_indextank_worker.rb
+++ lib/indextank.com/mongo_to_indextank_worker.rb
@@ -1,5 +1,5 @@
-# Sample worker that connects to MongoDB and iterates through a Mongo collection and puts all of the items into IndexTank for
-# full text awesomeness searching.
+# Sample worker that connects to MongoDB and iterates through a Mongo collection
+# and puts all of the items into IndexTank for full text awesomeness searching.
 
 require 'simple_worker'
 require 'mongoid'
@@ -10,13 +10,15 @@ class MongoToIndextankWorker < SimpleWorker::Base
   merge_gem 'indextank'
   merge 'person'
 
+  # These values are passed in to make it easy to run the example. Some values (such
+  # as index) could be placed in initialize() or directly inline to simplify things
   attr_accessor :mongo_host, :mongo_port, :mongo_username, :mongo_password,
                 :mongo_db_name,
-                :indextank_url
+                :indextank_url, :indextank_index
 
 
   def run
-    init_mongo
+    init_mongodb
     init_indextank
 
     log "saving person..."
@@ -26,7 +28,7 @@ class MongoToIndextankWorker < SimpleWorker::Base
 
     sleep 1
 
-    @index = @indextank.indexes('test_peeps')
+    @index = @indextank.indexes(indextank_index)
 
     log "querying persons..."
     persons = Person.find(:all, :conditions=>{:first_name=>"Ludwig"})
@@ -43,8 +45,8 @@ class MongoToIndextankWorker < SimpleWorker::Base
   end
 
 
-  # Configures smtp settings to send email.
-  def init_mongo
+  # Configures the MongoHQ settings using the Mongoid gem.
+  def init_mongodb
     Mongoid.configure do |config|
       config.database = Mongo::Connection.new(mongo_host, mongo_port).db(mongo_db_name)
       config.database.authenticate(mongo_username, mongo_password)
--- lib/indextank.com/run_mongo_to_indextank_worker.rb
+++ lib/indextank.com/run_mongo_to_indextank_worker.rb
@@ -1,15 +1,23 @@
 # TO USE:
-# Create a file in this directory called config.yml with the following parameters:
-
-#sw_access_key: ABC
-#sw_secret_key: XYZ
-#mongo_host: abc.mongohq.com
-#mongo_port: 27066
-#mongo_username: ABC
-#mongo_password: XYZ
-#mongo_db_name: workertest # name of database in your mongo instance
-#indextank_url: http://:MYTANKID+@1j.api.indextank.com
-
+# This example builds on the MongoDB example so check out that one first.
+#
+# 1) Get accounts/credentials for SimpleWorker, IndexTank, and a MongoDB (MongoHQ is pretty cool).
+#
+# 2) Create a file in this directory called config.yml with the following parameters:
+#
+# (Note: Your keys, urls, and names will be different than below. Get the right values from each service.)
+#
+# sw_access_key: ABC
+# sw_secret_key: XYZ
+# mongo_host: somehostname.mongohq.com
+# mongo_port: somenumber
+# mongo_username: ABC
+# mongo_password: XYZ
+# mongo_db_name: yourdbname
+# indextank_url: http://someaddress.api.indextank.com
+# indextank_index: yourindexname
+#
+#
 
 require 'simple_worker'
 require_relative "mongo_to_indextank_worker.rb"
@@ -24,19 +32,34 @@ SimpleWorker.configure do |config|
 end
 
 
-tw               = MongoToIndextankWorker.new
-tw.mongo_host   = @config["mongo_host"]
-tw.mongo_port = @config["mongo_port"]
-tw.mongo_username = @config['mongo_username']
-tw.mongo_password = @config['mongo_password']
-tw.mongo_db_name = @config["mongo_db_name"]
-tw.indextank_url = @config['indextank_url']
+# Some of these items could be placed in the worker directly.
+# They're included here to make easier to get the example running.
+itw               = MongoToIndextankWorker.new
+itw.mongo_host   = @config["mongo_host"]
+itw.mongo_port = @config["mongo_port"]
+itw.mongo_username = @config['mongo_username']
+itw.mongo_password = @config['mongo_password']
+itw.mongo_db_name = @config["mongo_db_name"]
+itw.indextank_url = @config['indextank_url']
+itw.indextank_index = @config['indextank_index']
 
-#tw.run_local
-tw.queue
 
-status = tw.wait_until_complete
+# Run the job (with several alternatives included)
+#
+#itw.run_local
+#itw.schedule(:start_at => 2.minutes.since, :run_every => 60, :run_times => 2)
+#itw.queue(:priority=>2)
+
+itw.queue
+
+
+# This gets stats on the job after completion. You can see the same in the
+# SimpleWorker dashboard.
+#
+# Note that wait_until_complete has issues with run_local and schedule.
+# Included for use with .queue so that we can display the log file.
+#
+status = itw.wait_until_complete
 p status
-puts tw.get_log
-
+puts itw.get_log
 
