7c06e6a60108397090b9edf9093eb5f0b4de9334
Luke Carpenter <orlyowlizere@gmail.com>
2011-05-25 21:10:27 +0100

    runner retool complete

--- lib/notifo/notifo_runner.rb
+++ lib/notifo/notifo_runner.rb
@@ -14,18 +14,21 @@ w.message = @y["message"]
 
 if @y["runlocal"] == true
   w.run_local
+
+  puts
+  puts "I was told to run locally... no magic for you :("
 else
   w.queue
+
+  puts "Job queued..."
+  puts "I'll block until it's finished"
+
+  w.wait_until_complete
+
+  puts "Yay it finished!"
+  puts "Here is it's log:"
+
+  puts w.log
 end
-
-puts "Job queued..."
-puts "I'll block until it's finished"
-
-w.wait_until_complete
-
-puts "Yay it finished!"
-puts "Here is it's log:"
-
-puts w.log
 
 puts "I exit now"
