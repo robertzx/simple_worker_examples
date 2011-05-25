240647f9a278e007757667229aa508d484cb829b
Luke Carpenter <orlyowlizere@gmail.com>
2011-05-25 20:34:04 +0100

    a really bad notifo example, gonna rework it...

--- lib/notifo/notifo_worker.rb
+++ lib/notifo/notifo_worker.rb
@@ -6,22 +6,34 @@ module SimpleWorker
 
     merge_gem "notifo"
 
-    # These are dynamic, and will be different for each user
-    attr_accessor :username, :message
+    # These are dynamic, and will be different for each task
+    attr_accessor :username, :message, :task
 
-    # These should be static, but are accessors for the purposes of keeping API keys secret
+    # These should be static, but are accessors for keeping API keys secret
     # Sort of like ENV vars in Heroku
     attr_accessor :notifo_user, :notifo_api
 
     # The SimpleWorker environment will invoke and run this def:
     def run
-      
+      notifo
+
+      case @task
+        when :subscribe then subscribe
+        when :send then send
+      end
+
     end
 
     def notifo
       @notifo = Notifo.new(@notifo_user, @notifo_api)
     end
 
+    def subscribe
+      @notifo.subscribe_user(@username)
+    end
+
+    def send
+      @notifo.post(@username, @message)
     end
   end
 end
