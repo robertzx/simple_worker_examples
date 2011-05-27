f5e9ef5160fb3b946bc675e010294cfebe770839
Luke Carpenter <orlyowlizere@gmail.com>
2011-05-27 20:03:36 +0100

    Redis example finished! ;D

--- lib/redis/redis_runner.rb
+++ lib/redis/redis_runner.rb
@@ -1,12 +1,15 @@
 require "simple_worker"
+require "yaml"
 require_relative "redis_worker"
 
+@y = YAML.load_file("redis.yml")
+
 SimpleWorker.configure do |config|
-  config.access_key = '1f4a1dbfaff1f8ea4fe6f70735125e79'
-  config.secret_key = 'c21c1b14e7a7c2efadf3bd84fba2cbd3'
+  config.access_key = @y['simpleworker_access']
+  config.secret_key = @y['simpleworker_secret']
 end
 
-w = SimpleWorker::Examples::RedisWorker.new
-w.redis_connection = ENV['REDIS']
+w = RedisWorker.new
+w.redis_connection = @y['redis']
 
 w.queue
