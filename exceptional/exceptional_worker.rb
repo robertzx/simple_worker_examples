923bf5e2351869bad79d36b364c0eacb73ac98bc
Roman Kononov <roman@appoxy.com>
2011-07-12 16:02:30 +0600

    getexceptional example

---
+++ exceptional/exceptional_worker.rb
@@ -0,0 +1,14 @@
+require 'simple_worker'
+require 'rubygems'
+class ExceptionalWorker< SimpleWorker::Base
+  attr_accessor :api_key
+  merge_gem 'exceptional'
+  def run
+    Exceptional::Config.api_key = api_key
+    Exceptional::Config.enabled=true
+    Exceptional.rescue do
+      raise "Exception should be sent to getexceptional"
+    end
+  end
+end
+
