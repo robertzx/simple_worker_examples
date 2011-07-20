fe1be13d283a2b19b8b1cacf2f1196aec2484611
Luke Carpenter <luke.carpenter@appoxy.com>
2011-07-20 21:31:00 +0100

    More coffee is required, filename screwup

--- sdb_tester/sdb_test_runner.rb
+++ sdb_tester/sdb_test_runner.rb
@@ -1,11 +1,4 @@
-class SDBTestWorker < SimpleWorker::Base
-  attr_accessor :aws_access, :aws_secret
-  merge_gem "right_aws"
-  def run
-    log "connecting to AWS..."
-    @sdb = RightAWS::SdbInterface.new(@aws_access, @aws_secret)
-    log "All OK!\nGetting domains..."
-    @sdb.list_domains[:domains].each{|x| log x.inspect }
-    log "done!"
-  end
-end
+require "simple_worker"
+require "yaml"
+
+
