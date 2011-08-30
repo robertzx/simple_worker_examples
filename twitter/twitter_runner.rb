45db21d9cade96621913f52972b346425bd6dec7
Chad Arimura <chad@appoxy.com>
2011-08-29 21:10:39 -0700

    - small changes to twitter worker

--- twitter/twitter_runner.rb
+++ twitter/twitter_runner.rb
@@ -1,20 +1,25 @@
 require "simple_worker"
 require "yaml"
 require "./twitter_worker"
+require 'active_support/core_ext'
 
-@y = YAML.load_file('./twitter.yml')
-
-@sw = @y['simpleworker']
+y = YAML.load_file('./twitter.yml')
 
 SimpleWorker.configure do |c|
-  c.access_key = @sw['api']
-  c.secret_key = @sw['secret']
+  c.access_key = y['simpleworker']['api']
+  c.secret_key = y['simpleworker']['secret']
 end
 
+w = TwitterWorker.new
 
+print "I am: "
+w.message = gets.chomp
 
-@w = TwitterWorker.new
-print "How are you feeling today?: "
-@w.message = gets.chomp
-@w.twitter_config = @y['twitter']
-@w.queue(:priority => 2)
+w.twitter_config = y['twitter']
+
+w.queue(:priority => 2)
+
+w.schedule(:start_at => 1.minutes.from_now,
+           :run_every => 60, # seconds
+           :run_times => 3,
+           :priority => 2)
